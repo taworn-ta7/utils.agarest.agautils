1baef7a7ea5236cb0b5afdc6679b84647b9ba0da
Taworn Ta <taworn.ta7@gmail.com>
2023-01-23 11:59:16 +0700

    - create AgaUtils by copy template

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# NOT COMPLETED
+
 # Agarest: Generations of War Utilities
+
+# Thank
+
+for template:
+https://github.com/nwutils
 
---
+++ agautils/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>Agarest Utilities</h1>
+  </header>
+
+  <main>
+    {{ hello }}
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: function () {
+    return {
+      hello: "Hello, Agarest :)"
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
