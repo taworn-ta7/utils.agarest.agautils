932bdaa31b37d81f3081d937b84394b75c3a0bac
Taworn Ta <taworn.ta7@gmail.com>
2023-01-29 08:46:34 +0700

    - load all data in agautils

--- agautils/src/stores/DataStore.js
+++ agautils/src/stores/DataStore.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const YAML = require('yaml');
 import { defineStore } from 'pinia';
 import Utils from '../helpers/utils';
 
@@ -9,6 +11,9 @@ export const useDataStore = defineStore({
 	id: 'DataStore',
 
 	state: () => ({
+		characterList: [],
+		weaponGroupList: [],
+		combinationSkillList: null,
 	}),
 
 	actions: {
@@ -16,11 +21,33 @@ export const useDataStore = defineStore({
 		 * Loads all data.
 		 */
 		async setup() {
-			console.log(JSON.stringify(Utils.characterList, null, 2));
+			this.characterList = [];
+			for (let i = 0; i < Utils.characterList.length; i++) {
+				const name = Utils.characterList[i];
+				const doc = this.loadDataFile(`./data/Character ${name}.yaml`);
+				this.characterList.push(doc);
+			}
+
+			this.weaponGroupList = [];
+			for (let i = 0; i < Utils.weaponGroupList.length; i++) {
+				const name = Utils.weaponGroupList[i];
+				const doc = this.loadDataFile(`./data/Weapon ${name} List.yaml`);
+				this.weaponGroupList.push(doc);
+			}
+
+			{
+				const doc = this.loadDataFile(`./data/Combination Skill List.yaml`);
+				this.combinationSkillList = doc;
+			}
 		},
 
 		// ----------------------------------------------------------------------
 
+		loadDataFile(file) {
+			const fh = fs.readFileSync(file, 'utf8');
+			const doc = YAML.parse(fh);
+			return doc;
+		}
 	},
 
 });
